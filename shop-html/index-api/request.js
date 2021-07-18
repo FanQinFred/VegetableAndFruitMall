53873dc7cb0d7e6f90141f1f34b9bf54b9d390b5
FanQinFred <1836459118@qq.com>
2021-07-18 20:00:57 +0800

    Update request.js

--- shop-html/index-api/request.js
+++ shop-html/index-api/request.js
@@ -22,7 +22,7 @@ service.interceptors.request.use(
         //向请求头添加参数： config.headers['userId']='xxx'
         //为请求头对象，添加token验证的Authorization字段
         config.headers.token = window.sessionStorage.getItem("token");
-        config.headers.username = window.sessionStorage.getItem("username");
+        config.headers.userid = window.sessionStorage.getItem("userid");
         return config;
     },
     function (error) {
