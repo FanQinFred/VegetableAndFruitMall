29047537d37871e036d92886179d74b2115c3053
何俊亮 <1710763616@qq.com>
2021-07-18 19:06:48 +0800

    index和cart

--- shop-html/assets/js/index.js
+++ shop-html/assets/js/index.js
@@ -7,3 +7,54 @@ export function popularCategories(data) {
         data: data //左边的data是变量名(key)后台接受，右边的data是参数,如果两者名字相同可以只写一个
     })
 }
+
+export function ourProducts(data) {
+    return service.request({
+        method: 'get',
+        url: 'http://localhost:40000/index/ourProducts',
+        data: data //左边的data是变量名(key)后台接受，右边的data是参数,如果两者名字相同可以只写一个
+    })
+}
+
+export function whatPeopleSay(data) {
+    return service.request({
+        method: 'get',
+        url: 'http://localhost:40000/index/whatPeopleSay',
+        data: data //左边的data是变量名(key)后台接受，右边的data是参数,如果两者名字相同可以只写一个
+    })
+}
+
+
+export function bestSellers(data) {
+    return service.request({
+        method: 'get',
+        url: 'http://localhost:40000/index/bestSellers',
+        data: data //左边的data是变量名(key)后台接受，右边的data是参数,如果两者名字相同可以只写一个
+    })
+}
+
+
+export function fromTheBlog(data) {
+    return service.request({
+        method: 'get',
+        url: 'http://localhost:40000/index/blog',
+        data: data //左边的data是变量名(key)后台接受，右边的data是参数,如果两者名字相同可以只写一个
+    })
+}
+
+
+export function newArrivals(data) {
+    return service.request({
+        method: 'get',
+        url: 'http://localhost:40000/index/newArrival',
+        data: data //左边的data是变量名(key)后台接受，右边的data是参数,如果两者名字相同可以只写一个
+    })
+}
+
+export function topRated(data) {
+    return service.request({
+        method: 'get',
+        url: 'http://localhost:40000/index/topRated',
+        data: data //左边的data是变量名(key)后台接受，右边的data是参数,如果两者名字相同可以只写一个
+    })
+}
