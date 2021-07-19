67668f4775505290545bdcd0c2c2d91797347563
hxy <862986375@qq.com>
2021-07-19 21:57:12 +0800

    feat: 增加redis配置文件

--- my-vspace/cache-service/src/main/java/cn/cqu/vspace/service/impl/CacheServiceImpl.java
+++ my-vspace/cache-service/src/main/java/cn/cqu/vspace/service/impl/CacheServiceImpl.java
@@ -5,15 +5,19 @@ import org.apache.dubbo.config.annotation.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import java.util.concurrent.TimeUnit;
+
 @Service
 public class CacheServiceImpl implements CacheService {
     //自动装配一个redisTemplate模板对象
     @Autowired
     private RedisTemplate<String,String> template;
+
     @Override
     public void insertCache(String key, String value) {
         //将json对象存储到redis
         template.opsForValue().set(key,value);
+        template.expire(key,15*60, TimeUnit.SECONDS);
     }
     @Override
     public String getDataByKey(String key) { //调用redis模型根据key获取对于的值
