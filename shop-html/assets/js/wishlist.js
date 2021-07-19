4bac15d7ccceb49a10f3958cf1d76044712dc722
FanQinFred <1836459118@qq.com>
2021-07-19 15:32:12 +0800

    wish

--- shop-html/assets/js/wishlist.js
+++ shop-html/assets/js/wishlist.js
@@ -7,3 +7,20 @@ export function wishlist(data) {
         data: data //左边的data是变量名(key)后台接受，右边的data是参数,如果两者名字相同可以只写一个
     })
 }
+
+export function getInfoById(data) {
+        return service.request({
+            method: 'post',
+            url: 'http://localhost:40000/shop/getInfoById',
+            data: data //左边的data是变量名(key)后台接受，右边的data是参数,如果两者名字相同可以只写一个
+        })
+    }
+
+
+    export function AddWish(data) {
+        return service.request({
+            method: 'post',
+            url: 'http://localhost:40000/shop/wish',
+            data: data //左边的data是变量名(key)后台接受，右边的data是参数,如果两者名字相同可以只写一个
+        })
+    }
