f7fc5f8ba9766f0025fb1fc7bbe784b827406ad1
xjq <879891091@qq.com>
2021-07-20 21:43:58 +0800

    fix: fix bug

--- my-vspace/vspace-web/src/main/java/cn/cqu/vspace/conroller/OrderController.java
+++ my-vspace/vspace-web/src/main/java/cn/cqu/vspace/conroller/OrderController.java
@@ -4,10 +4,8 @@ package cn.cqu.vspace.conroller;
 import cn.cqu.vspace.service.OrderService;
 import com.alibaba.fastjson.JSONObject;
 import org.apache.dubbo.config.annotation.Reference;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/order")
@@ -16,19 +14,21 @@ public class OrderController {
     @Reference
     OrderService orderService;
     @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public JSONObject addOrder(String token, String orderId, String goodsId, String status, String total){
+    public JSONObject addOrder(@RequestHeader("token") String token, @Param("orderId") String orderId,
+                               @Param("goodsId") String goodsId, @Param("status") String status, @Param("total") String total){
         return orderService.addOrder(token, orderId, goodsId, status, total);
     }
     @RequestMapping(value = "/delete", method = RequestMethod.POST)
-    public JSONObject deleteOrder(String token, String orderId){
+    public JSONObject deleteOrder(@RequestHeader("token") String token, @Param("orderId") String orderId){
         return orderService.deleteOrder(token, orderId);
     }
     @RequestMapping(value = "/show", method = RequestMethod.POST)
-    public JSONObject showOrders(String token){
+    public JSONObject showOrders(@RequestHeader("token") String token){
         return orderService.showOrders(token);
     }
     @RequestMapping(value = "/update", method = RequestMethod.POST)
-    public JSONObject updateOrder(String token, String orderId, String goodsId, String status, String total){
+    public JSONObject updateOrder(@RequestHeader("token") String token, @Param("orderId") String orderId,
+                                  @Param("goodsId") String goodsId, @Param("status") String status, @Param("total") String total){
         return orderService.updateOrder(token, orderId, goodsId, status, total);
     }
 }
