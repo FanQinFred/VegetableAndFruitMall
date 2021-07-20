7e29810dbaf279566bbc09d63fc60aaabd0b03a7
FanQinFred <1836459118@qq.com>
2021-07-20 22:48:26 +0800

    逗号

--- my-vspace/vspace-web/src/main/java/cn/cqu/vspace/conroller/OrderController.java
+++ my-vspace/vspace-web/src/main/java/cn/cqu/vspace/conroller/OrderController.java
@@ -13,22 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/order")
 @CrossOrigin
 public class OrderController {
-    @Reference
-    OrderService orderService;
-    @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public JSONObject addOrder(String token, String orderId, String goodsId, String status, String total){
-        return orderService.addOrder(token, orderId, goodsId, status, total);
-    }
-    @RequestMapping(value = "/delete", method = RequestMethod.POST)
-    public JSONObject deleteOrder(String token, String orderId){
-        return orderService.deleteOrder(token, orderId);
-    }
-    @RequestMapping(value = "/show", method = RequestMethod.POST)
-    public JSONObject showOrders(String token){
-        return orderService.showOrders(token);
-    }
-    @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public JSONObject updateOrder(String token, String orderId, String goodsId, String status, String total){
-        return orderService.updateOrder(token, orderId, goodsId, status, total);
-    }
+//    @Reference
+//    OrderService orderService;
+//    @RequestMapping(value = "/add", method = RequestMethod.POST)
+//    public JSONObject addOrder(String token, String orderId, String goodsId, String status, String total){
+//        return orderService.addOrder(token, orderId, goodsId, status, total);
+//    }
+//    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+//    public JSONObject deleteOrder(String token, String orderId){
+//        return orderService.deleteOrder(token, orderId);
+//    }
+//    @RequestMapping(value = "/show", method = RequestMethod.POST)
+//    public JSONObject showOrders(String token){
+//        return orderService.showOrders(token);
+//    }
+//    @RequestMapping(value = "/add", method = RequestMethod.POST)
+//    public JSONObject updateOrder(String token, String orderId, String goodsId, String status, String total){
+//        return orderService.updateOrder(token, orderId, goodsId, status, total);
+//    }
 }
