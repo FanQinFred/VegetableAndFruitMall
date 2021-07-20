e6af74fdb48cee413ca6f50d53dc31c87793f1ee
xjq <879891091@qq.com>
2021-07-20 16:34:47 +0800

    feat: add wishListController

---
+++ my-vspace/vspace-web/src/main/java/cn/cqu/vspace/conroller/WishListController.java
@@ -0,0 +1,31 @@
+package cn.cqu.vspace.conroller;
+
+import cn.cqu.vspace.service.WishListService;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/wishlist")
+public class WishListController {
+
+    @Reference
+    WishListService wishListService;
+
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    public JSONObject deleteGoodsInWishList(@RequestHeader("token") String token, @Param("id") String id){
+        String[] goods = id.split("-");
+        List<Integer> goodsIdList = new ArrayList<>();
+        for (String good : goods) {
+            goodsIdList.add(Integer.parseInt(good));
+        }
+        return wishListService.deleteGoodsInWishList(token, goodsIdList);
+    }
+}
