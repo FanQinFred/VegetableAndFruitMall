a9f218aeaa612dbd961f63c2d5f3cdd05e7af4a2
FanQinFred <1836459118@qq.com>
2021-07-22 10:55:05 +0800

    cart

--- shop-html/assets/js/cart.js
+++ shop-html/assets/js/cart.js
@@ -3,7 +3,7 @@ import service from "./request.js"
 export function apply_Coupon(data) {
     return service.request({
         method: 'get',
-        url: 'http://localhost:40000/cart/apply_Coupon',
+        url: 'http://fanqin.vaiwan.com/cart/apply_Coupon',
         data: data //左边的data是变量名(key)后台接受，右边的data是参数,如果两者名字相同可以只写一个
     })
 }
@@ -11,7 +11,7 @@ export function apply_Coupon(data) {
 export function updata(data) {
     return service.request({
         method: 'get',
-        url: 'http://localhost:40000/cart/updata',
+        url: 'http://fanqin.vaiwan.com/cart/updata',
         data: data //左边的data是变量名(key)后台接受，右边的data是参数,如果两者名字相同可以只写一个
     })
 }
